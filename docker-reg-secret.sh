6c68baa7b8ab51d852baac6374cbadaf6b572add
HrithikKanugula <hrithik.kanugula@opsmx.io>
2025-07-04 21:52:51 -0700

    test

--- docker-reg-secret.sh
+++ docker-reg-secret.sh
@@ -1,5 +1,5 @@
 sudo k8s kubectl -n kaniko create secret docker-registry srinidocker \
   --docker-server="https://index.docker.io/v1/" \
-  --docker-username=ksrinimba \
-  --docker-password="Kambh12#" \
+  --docker-username=NNNNNN \
+  --docker-password="XXXXXX" \
   --docker-email=ksrinimba@gmail.com
